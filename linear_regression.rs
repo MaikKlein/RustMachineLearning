729ff92013a2a679cab6fb4a4d410c84cefd9d64
Maik Klein <maik@maik.fritz.box>
2013-08-04 20:38:36 +0200

    tab

--- linear_regression.rs
+++ linear_regression.rs
@@ -64,8 +64,8 @@ fn main() {
                  HouseData::new(20.0f,40.0f),
                  HouseData::new(30.0f,60.0f),
                  HouseData::new(40.0f,80.0f),
-                 HouseData::new(50.0f,100.0f),
-                 HouseData::new(60.0f,120.0f)];
+                 HouseData::new(50.0f,100.0f),	
+				 HouseData::new(60.0f,120.0f)];
 
  printfln!(linear_regression(0.00001f,//stepsize
  0.0000001f,//epsilon
